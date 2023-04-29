49d141606d40f0a8b7ac4adc3068660e12740100
baifengxiao <javaer@111.com>
2023-04-29 21:17:31 +0800

    Revert "添加微博热搜"
    This reverts commit 52d3f36925cafc9cf9c55d97d75e967b7eb4a19e.

--- source/js/census.js
+++ source/js/census.js
@@ -2,9 +2,9 @@ var start_date = '20220901' // 开始日期
 var date = new Date();
 var end_date = '' + date.getFullYear() + (date.getMonth() > 8 ? (date.getMonth() + 1) : ("0" + (date.getMonth() + 1))) + (date.getDate() > 9 ? date.getDate() : ("0" + date.getDate())); // 结束日期
 
-var access_token = '121.77a43a0bd3e286e0dd9691a71a52a6c2.YHvxPma5_MCPUZEAaJzjC4mDbf0EYbX3LpWmcKw.Qzw62Q' // accessToken
-var site_id = '19531142' // 网址 id
-var dataUrl = 'https://baidu-tongji-api.vercel.app/api?access_token=' + access_token + '&site_id=' + site_id
+var access_token = '' // accessToken
+var site_id = '' // 网址 id
+var dataUrl = '' + access_token + '&site_id=' + site_id
 // var dataUrl = 'https://baidu-tongji.fomal.cc/api?site_id=' + site_id
 var metrics = 'pv_count' // 统计访问次数 PV 填写 'pv_count'，统计访客数 UV 填写 'visitor_count'，二选一
 var metricsName = (metrics === 'pv_count' ? '访问次数' : (metrics === 'visitor_count' ? '访客数' : ''))
